6827a8d99f6ea6d52c31497403b316fb060c1922
lester jimenez <lesterjimenezestacuy@hotmail.es>
2024-10-22 00:25:54 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const app = require('./server.js');  // Importa la aplicación de Express desde `server.js`
+const app = require('./server.js');  
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {

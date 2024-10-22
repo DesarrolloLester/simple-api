519f287652d00b71c76e47ce32e6736062d1caf9
lester jimenez <lesterjimenezestacuy@hotmail.es>
2024-10-22 00:31:27 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const app = require('./server.js');
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Servidor escuchando en http://localhost:${port}`);
+    console.log(`Servidor escuchando en puerto:${port}`);
 });
